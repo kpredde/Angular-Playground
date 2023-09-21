79acbb4a1c99011e23c38c842ee0d9481ea9c724
kpredde <8453037+kpredde@users.noreply.github.com>
2023-09-21 07:57:12 -0400

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { z } from 'zod';
+import { z, ZodError } from 'zod';
+
+const AccountSchema = z.object({
+  id: z.number(),
+  code: z.string().nonempty(),
+  date: z.date(),
+  isAcitve: z.boolean(),
+});
+type IAccount = z.infer<typeof AccountSchema>;
 
 @Component({
   selector: 'my-app',
@@ -11,23 +19,18 @@ import { z } from 'zod';
   templateUrl: 'app-main.html',
 })
 export class App {
-  name = () => {
-    const AccountSchema = z.object({
-      id: z.number(),
-      code: z.string(),
-      date: z.date(),
-    });
-    type IAccount = z.infer<typeof AccountSchema>;
+  myObject = (): IAccount | ZodError => {
     const myDemoAccount: IAccount = {
       id: 1,
-      code: 'demo',
+      code: 'Demo',
       date: new Date(),
+      isAcitve: true,
     };
     const account = AccountSchema.safeParse(myDemoAccount);
     if (account.success) {
       return account.data;
     } else {
-      return account.error.message;
+      return account.error;
     }
   };
 }
